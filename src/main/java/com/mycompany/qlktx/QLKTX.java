59be3afff2bb332bcaa2663e21a4379b31afbc89
BacMV <bacmai0104nt@gmail.com>
2024-08-26 19:29:22 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/qlktx/QLKTX.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.qlktx;
+
+/**
+ *
+ * @author Amin
+ */
+public class QLKTX {
+    public static void main(String[] args) {
+        // Thêm mã nguồn khởi tạo của bạn tại đây
+        System.out.println("Hello, QLKTX!");
+    } 
+}
